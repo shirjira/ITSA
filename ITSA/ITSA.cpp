f6864098cb434da792dde75c0f22908d662b5cc8
語 墨 <jame8030@gmail.com>
2023-03-13 20:23:30 +0800

    停車費計算

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,15 +1,27 @@
 ﻿#include <iostream>
-#include<iomanip>
 using namespace std;
+
 int main()
 {
-	double x = 0.0;
-	double result = 0.0;
+	int starH=0, starM=0,endH=0,endM=0;
 
-	cin >> x;
-	result = x * 1.6;
-	cout << setprecision(1) << fixed << result << endl;
+	cin >> starH >> starM;
+	cin >> endH >> endM;
+	int time = (endH * 60 + endM) - (starH * 60 + starM);
+	if(time <=120 && time >=0)
+	{
+		cout << time/30 * 30 << endl;
+	}
+	else if (time > 120 && time <= 240)
+	{
+		cout << (time - 120) / 30 * 40 + 120 << endl;
+    }
+	else 
+	{
+		cout << (time - 240) / 30 * 60 + 120+160 << endl;
+	}
 	return 0;
 
 }
+
 
