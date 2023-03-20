5386c28f380cd0c7c38ec5706358062dfcfb15a5
語 墨 <jame8030@gmail.com>
2023-03-21 01:13:46 +0800

    複數運算

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,35 +1,68 @@
 ﻿#include <iostream>
-
 using namespace std;
+
+class Complex {
+private:
+	int re, im;
+	// re:實部
+	// im:虛部
+public:
+	Complex(int r = 0, int i = 0) {
+		re = r;
+		im = i;
+	}	
+		Complex operator + (Complex const& obj) {
+			Complex result;
+			result.re = re + obj.re;
+			result.im = im + obj.im;
+
+			return result;		
+	}
+		Complex operator - (Complex const& obj) {
+			Complex result;
+			result.re = re - obj.re;
+			result.im = im - obj.im;
+			return result;
+		}
+		Complex operator * (Complex const& obj) {
+			Complex result;
+			//a1=re,b1=im
+			//a2=obj.re,b2=obj.im
+			result.re = re * obj.re - im * obj.im; //a1*a2-b1*b2
+			result.im = re * obj.im + obj.im * im;//a1*b2 +a2*b1
+			return result;
+		}
+	void display() {
+		cout << re << " " << im << endl;
+	}
+};
 
 int main()
 {
-	int mooth;
-	string season;
-	cin >> mooth;
-	switch (mooth)
-	{
-	case 3:
-	case 4:
-	case 5:
-		season = "Spring";
-		break;
-	case 6:
-	case 7:
-	case 8:
-		season = "Summer";
-		break;
-	case 9:
-	case 10:
-	case 11:
-		season = "Autumn";
-		break;
-	case 12:
-	case 1:
-	case 2:
-		season = "Winter";
-		break;
-	}
-	cout << season << endl;
+	int n;
+	cin >> n;
 
+	Complex c[10];
+	for (int i = 0;i < n;i++) {
+		char oper;
+		int ar, ai, br, bi;
+		cin >> oper >> ar >> ai >> br >> bi;
+		Complex a(ar, ai);
+		Complex b(br, bi);
+
+		switch (oper){
+		    case '+' :
+		    	c[i] = a + b;
+		    	break;
+		    case '-':
+			    c[i] = a - b;
+			    break;
+		    case '*':
+			    c[i] = a * b;
+			    break;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		c[i].display();
+	}
 }
