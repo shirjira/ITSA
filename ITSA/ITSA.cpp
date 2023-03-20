ab748f53c0c0b34cc36b08f5490727311d225df9
語 墨 <jame8030@gmail.com>
2023-03-21 01:44:49 +0800

    計算正整數被3整除之數值之總和

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -4,23 +4,14 @@ using namespace std;
 
 int main()
 {
-	int a;
-	int b;
-	
-	cin >> a;
-	for (b = 2; b < a; b++) {
-		if (a % b == 0) {
-			break;
+	int n;
+	int sum = 0;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) { //1到N之間的數
+		if (i % 3 == 0) { //被3整除的數字總和
+			sum += i;
 		}
 	}
-	//是質數輸出"YES"
-	if (b == a) {
-		cout << "YES" << endl;
-	}
-	//不是質數輸出"NO"
-	else
-	{
-		cout << "NO" << endl;
-	}
+	cout << sum << endl;
 }
-
