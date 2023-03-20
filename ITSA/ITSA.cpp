d73b93fcf23d63f0b9e2b44cbe1e538d45c7e5bd
語 墨 <jame8030@gmail.com>
2023-03-21 01:16:23 +0800

    複數運算

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,68 +1,31 @@
 ﻿#include <iostream>
 using namespace std;
 
-class Complex {
-private:
-	int re, im;
-	// re:實部
-	// im:虛部
-public:
-	Complex(int r = 0, int i = 0) {
-		re = r;
-		im = i;
-	}	
-		Complex operator + (Complex const& obj) {
-			Complex result;
-			result.re = re + obj.re;
-			result.im = im + obj.im;
-
-			return result;		
-	}
-		Complex operator - (Complex const& obj) {
-			Complex result;
-			result.re = re - obj.re;
-			result.im = im - obj.im;
-			return result;
-		}
-		Complex operator * (Complex const& obj) {
-			Complex result;
-			//a1=re,b1=im
-			//a2=obj.re,b2=obj.im
-			result.re = re * obj.re - im * obj.im; //a1*a2-b1*b2
-			result.im = re * obj.im + obj.im * im;//a1*b2 +a2*b1
-			return result;
-		}
-	void display() {
-		cout << re << " " << im << endl;
-	}
-};
-
 int main()
 {
 	int n;
 	cin >> n;
-
-	Complex c[10];
-	for (int i = 0;i < n;i++) {
+	for (int i = 0; i < n; i++) {
 		char oper;
-		int ar, ai, br, bi;
-		cin >> oper >> ar >> ai >> br >> bi;
-		Complex a(ar, ai);
-		Complex b(br, bi);
+		int a, b, c, d;
+		cin >> oper >> a >> b >> c >> d;
 
-		switch (oper){
-		    case '+' :
-		    	c[i] = a + b;
-		    	break;
-		    case '-':
-			    c[i] = a - b;
-			    break;
-		    case '*':
-			    c[i] = a * b;
-			    break;
+		switch (oper) {
+		case '+':
+			cout << a + c << " " << b + d << endl;
+			break;
+		case '-':
+			cout << a - c << " " << b - d << endl;
+			break;
+		case '*':
+			cout << a * c - b * d << " " << b * c + a * d << endl;
+			break;
+		case '/':
+			cout << (a * c + b * d) / (c * c + d * d) << " " << (b * c - a * d) / (c * c + d * d) << endl;
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		c[i].display();
-	}
+	return 0;
+
+	
 }
+
