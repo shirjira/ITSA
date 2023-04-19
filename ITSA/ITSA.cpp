ac20e897090f5fd5291958a49d0d857de23cac15
語 墨 <jame8030@gmail.com>
2023-04-19 18:52:42 +0800

    最少派車數

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,48 +1,25 @@
-﻿#include <iostream>
-#include <string>
-#include <vector>
-#include <cstring>
-#include <cstdlib>
+﻿#include<iostream>
+#include<map>
 
 using namespace std;
 
 int main()
 {
-    string strN;
-    getline(cin, strN);
-    int N = atoi(strN.c_str());
+    int order, s, d;
+    int time[25] = { 0 };
+    int cars = 0;
+    cin >> order;
 
-    for (int g = 0; g < N; g++)
-    {
-        string STRinput;
-        getline(cin, STRinput);
-        string other = "!@#$%^&*()_++`1234567890-=={}||qwertyuiop[]\\:""asdfghjkl;''<>??zxcvbnm,.//";
-        int fir = STRinput.size();
-        int sec = other.size();
+    for (int i = 0; i < order; i++) {
 
-        for (int i = 0; i < fir; i++)
-        {
-            STRinput[i] = tolower(STRinput[i]);
-        }
-        for (int i = 0; i < fir; i++)
-        {
-            for (int j = 0; j < sec; j++)
-            {
-                if (STRinput[i] == ' ')
-                {
-                    STRinput[i] = ' ';
-                    break;
-                }
-                else if (STRinput[i] == other[j])
-                {
-                    STRinput[i] = other[j + 1];
-                    break;
-                }
-            }
-            cout << STRinput[i];
-        }
-        cout << endl;
+        cin >> s >> d;
+
+        for (int j = s; j < d; j++) {
+            time[j]++;
+            if (time[j] > cars)cars = time[j];
+        };
+
     }
+    cout << cars << endl;
     return 0;
 }
-
