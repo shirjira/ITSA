e43e407d349900f6f255ca0b254479ca8c8eed40
語 墨 <jame8030@gmail.com>
2023-04-19 14:20:37 +0800

    英文斷詞

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,31 +1,38 @@
-﻿#include<iostream>
-#include<string>
-
+﻿#include<iostream>  
+#include<string.h>  
+#include<ctype.h>  
 using namespace std;
 
-int appearNum(string& s, int p, string& l);
-
-int main()
-{
-    string inputS, inputL, save;
-
-    cin >> inputS >> inputL;
-    cout << appearNum(inputS, 0, inputL) << endl;
+int main() {
+    char enter[10000];
+    fgets(enter, 9999, stdin);
+    int len = strlen(enter);
+    for (int i = 0; i < len; i++) {
+        enter[i] = tolower(enter[i]);
+    }
+    char ans[1000][1000];
+    int nowAns = 0;
+    char* pch = strtok(enter, " \r\n");  
+    while (pch != NULL) {
+        int judge = 1;
+        for (int i = 0; i < nowAns; i++) {
+            if (strcmp(ans[i], pch) == 0) {
+                judge = 0;
+                break;
+            }
+        }
+        if (judge) {
+            strcpy(ans[nowAns], pch);
+            nowAns++;
+        }
+        pch = strtok(NULL, " \r\n");  
+    }
+    for (int i = 0; i < nowAns; i++) {
+        if (i)
+            cout<<" ";
+           cout<<ans[i];
+    }
+    cout<<"\n";
     return 0;
 }
-int appearNum(string& s, int p, string& l)
-{
-    int a = 0;
-    if (p > l.length())return a = 0;
-    for (int i = 0; i < s.length(); i++)
-    {
-        if (l[i + p] == s[i])continue;
-        else return a += appearNum(s, p + 1, l);
-    }
-    a++; 
-    return a += appearNum(s, p + 1, l);
-
-}
-
-	
 	
