7731e4eb4a886576e701a06500c00e14ba593847
語 墨 <jame8030@gmail.com>
2023-04-19 19:15:07 +0800

    圈圈叉叉

--- ITSA/ITSA.cpp
+++ ITSA/ITSA.cpp
@@ -1,25 +1,50 @@
-﻿#include<iostream>
-#include<iomanip>
-
+﻿#include<iostream> 
 using namespace std;
 
 int main()
 {
-    double a[10];
-    double max = 0.0, min = 0.0;
-    for (int i = 0; i < 10; i++)
+    int board[3][3];
+    int flag;
+    bool compare = false;
+    for (int i = 0; i < 3; i++) 
     {
-        cin >> a[i];
-    }
-    max = a[0];
-    min = a[0];
-    for (int i = 0; i < 10; i++) 
-    {
-        if (a[i] > max)max = a[i];
-        if (a[i] < min)min = a[i];
+        for (int j = 0; j < 3; j++) 
+        {
+            cin >> board[i][j];
+        }
     }
 
-    cout << "maximum:" << fixed << setprecision(2) << max << endl;
-    cout << "minimum:" << fixed << setprecision(2) << min << endl;
+    for (int i = 0; i < 2; i++) {
+        flag = 0;
+        for (int j = 0; j < 2; j++) //行比較 
+        {
+            if (board[i][j] == board[i][j + 1])flag++;
+        }
+        if (flag == 2)compare = true;
+    }
+
+    for (int i = 0; i < 2; i++) {
+        flag = 0;
+        for (int j = 0; j < 2; j++) //列比較 
+        {
+            if (board[j][i] == board[j + 1][i])flag++;
+        }
+        if (flag == 2)compare = true;
+    }
+    flag = 0;
+    for (int i = 0; i < 2; i++) //左上到右下 
+    {
+
+        if (board[i][i] == board[i + 1][i + 1])flag++;
+        if (flag == 2)compare = true;
+    }
+    flag = 0;
+    for (int i = 0; i < 2; i++) //右下到左上 
+    {
+        if (board[2 - i][i] == board[1 - i][i + 1])flag++;
+        if (flag == 2)compare = true;
+    }
+    if (compare == true)cout << "True" << endl;
+    else cout << "False" << endl;
     return 0;
 }
